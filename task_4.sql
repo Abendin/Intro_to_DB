1a48bdb36526eb96a62db81b3907d788efa4bb58
Abel Awenatei Abendin <abendinabel@gmail.com>
2025-08-22 23:54:34 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,10 @@
 -- task_4.sql
--- Print full description of the books table
-
 SELECT COLUMN_NAME,
        COLUMN_TYPE,
        IS_NULLABLE,
-       COLUMN_DEFAULT,
        COLUMN_KEY,
+       COLUMN_DEFAULT,
        EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books';
