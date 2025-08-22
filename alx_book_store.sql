b657abed63cbd373e1d2eb8e2bc0054ede52bbcd
Abel Awenatei Abendin <abendinabel@gmail.com>
2025-08-22 23:05:17 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,12 +3,12 @@ CREATE DATABASE alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
@@ -17,21 +17,21 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
